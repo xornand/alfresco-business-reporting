3b1e25ea639a297819c1b8da00bec3f6d1adcd42
tjarda peelen <tjarda.peelen@incentro.com>
2013-04-14 12:22:54 +0200

    another initial commit on another laptop :-( rev 0.8.0.1

--- source/data-dictionary/Scripts/reporting/createTables.js
+++
@@ -1,6 +0,0 @@
-<import resource="/Company Home/Data Dictionary/Scripts/reporting/alfresco-business-reporting.lib.js">
-// make sure we always include the reporting library 
-
-//var fileString = "person,folder,document,site,datalist,datalistitem,workflowdef,workflowtask,forum,topic,post";
-// otherwise, define a subset of the tables to create
-reporting.createEmptyTables(fileString);
